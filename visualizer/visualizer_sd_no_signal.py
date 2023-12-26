539437fa084990bbefcbeacdcd3f1285142dcb95
giangnguyen <gianghnguyen84@gmail.com>
2023-12-26 11:30:50 +0700

    adding inspection and pause per frame when visualizing

--- visualizer/visualizer_sd_no_signal.py
+++ visualizer/visualizer_sd_no_signal.py
@@ -1,69 +1,95 @@
-import pygame, math, serial, numpy
-pygame.init()
+import pygame, math, numpy
 
-#constant based on lidar resolution
+# constant based on lidar resolution
 LIDAR_RESOLUTION = 240
-#lidar resolution divided by 4 to simplify the visualization
+# lidar resolution divided by 4 to simplify the visualization
 VISUALIZATION_RESOLUTION = 240
+# Selected positions in a frame (result of the Sklearn SelectKBest function)
+POSITIONS_PER_FRAME = [141, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 203, 204, 205, 206, 207]
 
-distances_list = []
-first_run = True
 
-def GetDataFromArduino(line):
-    global distances_list, first_run
-    #[:-3] get rid of end of line sign and additional comma separator that is sent from arduino
+def get_data_from_arduino(line):
+    # [:-3] get rid of end of line sign and additional comma separator that is sent from arduino
     data = line[:-3]
     print(data)
-    d_list= data.split(",")
+    d_list = data.split(",")
     return d_list
 
 
-
-
-def GenerateLinePositions(numberOfLines):
-    angle = 360/numberOfLines
+def generate_line_positions(number_of_lines):
+    angle = 360 / number_of_lines
     lines = []
-    for x in range(numberOfLines):
-        lines.append([300 * math.cos((x+1)*angle/180 * math.pi), 300 * math.sin((x+1)*angle/180 * math.pi)])
+    for x in range(number_of_lines):
+        lines.append([300 * math.cos((x + 1) * angle / 180 * math.pi), 300 * math.sin((x + 1) * angle / 180 * math.pi)])
     return lines
 
 
+def run():
+    pygame.init()
 
-line_positions = GenerateLinePositions(VISUALIZATION_RESOLUTION)
+    line_positions = generate_line_positions(VISUALIZATION_RESOLUTION)
 
-# Set up the drawing window
-screen = pygame.display.set_mode([800, 800])
-sysfont = pygame.font.get_default_font()
-font1 = pygame.font.SysFont(sysfont, 72)
+    # Set up the drawing window
+    screen = pygame.display.set_mode([800, 800])
+    sys_font = pygame.font.get_default_font()
+    font1 = pygame.font.SysFont(sys_font, 72)
 
-file1 = open('I:/arduino lidar/visualizer/test6.txt', 'r')
-Lines = file1.readlines()
-
-for line in Lines:
-    distances = GetDataFromArduino(line)
-    print(len(distances))
-    if(len(distances) == LIDAR_RESOLUTION):
+    file1 = open('../data/test6.txt', 'r')
+    lines = file1.readlines()
+    running = True
+    counter = 0
+    paused = False
+    inspect_mode = False
+    while counter < len(lines):
+        line = lines[counter]
+        if inspect_mode:
+            paused = True
         # Did the user click the window close button?
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    paused = not paused
+                    print('PAUSE STATUS: {}'.format(paused))
+                elif event.key == pygame.K_i:
+                    """
+                    Press 'i' keyboard will turn on inspection mode,
+                    which run frame by frame
+                    """
+                    inspect_mode = not inspect_mode
+                    print('INSPECT MODE: {}'.format(inspect_mode))
+            elif event.type == pygame.QUIT:
+                # Press 'X' button on window will close the program
                 running = False
-            
-    
-        # Fill the background with white
-        screen.fill((250, 250, 250))
 
-    
-        for x in range(VISUALIZATION_RESOLUTION):
-            a = int(distances[x])/2000
-            if x in [141, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 203, 204, 205, 206, 207]:
-                pygame.draw.circle(screen, (255,0,0), (line_positions[x][0]*a+400, line_positions[x][1]*a+400), 3)
-            else:
-                pygame.draw.circle(screen, (0,0,0), (line_positions[x][0]*a+400, line_positions[x][1]*a+400), 2)
-            
-            
-        pygame.draw.circle(screen, (252, 132, 3), (400, 400), 12)
-        # Flip the display
-        pygame.display.flip()
-        pygame.time.wait(50)
+        if not running:
+            break
+        elif paused:
+            continue
 
-pygame.quit()
+        distances = get_data_from_arduino(line)
+        print(len(distances))
+        if len(distances) == LIDAR_RESOLUTION:
+            # Fill the background with white
+            screen.fill((250, 250, 250))
+
+            for x in range(VISUALIZATION_RESOLUTION):
+                a = int(distances[x]) / 2000
+                if x in POSITIONS_PER_FRAME:
+                    pygame.draw.circle(screen, (255, 0, 0),
+                                       (line_positions[x][0] * a + 400, line_positions[x][1] * a + 400), 3)
+                else:
+                    pygame.draw.circle(screen, (0, 0, 0),
+                                       (line_positions[x][0] * a + 400, line_positions[x][1] * a + 400),
+                                       2)
+
+            pygame.draw.circle(screen, (252, 132, 3), (400, 400), 12)
+            # Flip the display
+            pygame.display.flip()
+            pygame.time.wait(50)
+            counter += 1
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    run()
